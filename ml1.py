bb943aff43534670b01c6139705d47c2cc906550
pra8eek <prateek12499@gmail.com>
2018-10-09 16:38:01 +0530

    randon commit

--- ml1.py
+++ ml1.py
@@ -1,10 +1,20 @@
 from sklearn import tree
-# features - [weight,texture]
+feature_names = ['weight' ,'texture' ]
 # texture : 0-Bumpy ; 1-Smooth
 features = [ [130,1] , [140,1] , [150,0] , [170,0] ]
-# labels : 0-Apple ; 1-Orange
-labels =  [ 0 , 0 , 1 , 1 , 2 , 2 ]
+target_names = [ 'Apple' , 'Orange']
+labels =  [ 0 , 0 , 1 , 1  ]
 
 clf = tree.DecisionTreeClassifier()
 clf.fit( features , labels )
-print clf.predict( [[150 , 0]] )
+print clf.predict( [[147 , 1]] )
+
+#Visualising the tree
+import graphviz 
+dot_data = tree.export_graphviz(clf, out_file=None, 
+                         feature_names=feature_names,  
+                         class_names=target_names,  
+                         filled=True, rounded=True,  
+                         special_characters=True)    
+graph = graphviz.Source(dot_data) 
+graph.render("apples_and_oranges") 
--- ml2.py
+++ ml2.py
@@ -20,13 +20,11 @@ print test_target
 print clf.predict( test_data )
 
 #Visualising the tree
-import graphviz
-dot_data = tree.export_graphviz(clf, out_file=None) 
+import graphviz 
+dot_data = tree.export_graphviz(clf, out_file=None, 
+                         feature_names=iris.feature_names,  
+                         class_names=iris.target_names,  
+                         filled=True, rounded=True,  
+                         special_characters=True)    
 graph = graphviz.Source(dot_data) 
 graph.render("iris") 
-dot_data = tree.export_graphviz(clf, out_file=None, 
-                         feature_names = iris.feature_names,  
-                         class_names = iris.target_names,  
-                         filled = True, rounded = True,  
-                         special_characters=True)  
-graph = graphviz.Source(dot_data)  

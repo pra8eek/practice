31a38ad320d877317ed450f76aab36f5a75c58ff
pra8eek <prateek12499@gmail.com>
2018-10-26 12:01:02 +0530

    Emojis added

--- tree.py
+++ tree.py
@@ -1,4 +1,3 @@
-from sklearn import tree
 class Node :
     def __init__( self , num = None ) :
         self.val = num
@@ -67,18 +66,32 @@ class Tree :
                 self.Postorder(node.left)
                 self.Postorder(node.right)
                 print(node.val , end= " ")
+    def search( self , num , node = None) :
+        if self.isEmpty() :
+            print("Khali tree me kya apni gand dhund raha hai bhosdike! \U0001F602 \U0001F602 \U0001F602")
+            return
+        if node == None :
+            node = self.root
+        if node.val == num :
+            print("Element found") 
+            return
+        elif node.val > num :
+            if node.left == None :
+                print("Element not found")
+            else :
+                self.search( num , node.left )
+        else :
+            if node.right == None :
+                print("Element not found")
+            else :
+                self.search( num , node.right )
 
 tt = Tree()
-tt.insert(5)
-tt.insert(3)
-tt.insert(7)
-tt.insert(2)
-tt.insert(6)
-tt.insert(9)
 print("1. Insert")
 print("2. Preorder Traversal")
 print("3. Inorder Traversal")
 print("4. Postorder Traversal")
+print("5. Search for a element")
 print("0. Exit")
 n = -1
 while n!=0 :
@@ -87,14 +100,20 @@ while n!=0 :
         print("Sad that you quit, cuz that's what losers do bitch")
         break
     elif n == 1 :
-        print("Enter the value to be inserted : ",end="")
-        tt.insert(int(input()))
+        try :
+            print("Enter the value to be inserted : ",end="")
+            tt.insert(int(input()))
+        except :
+            print("\U0001F595")
     elif n == 2 :
         tt.traverse(n-1)
     elif n == 3 :
         tt.traverse(n-1)
     elif n == 4 :
         tt.traverse(n-1)
+    elif n == 5 :
+        print("Kya search karna chahenge aap : ",end="")
+        tt.search(int(input()))
     else :
         print("Chutiya hai kya! Fir se daal")
     print("-----------------------------------------------------------")
